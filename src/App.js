03a3cb985b4550d622f78ef29f37dcb019ec4ba1
blur <thetobiterra@gmail.com>
2023-03-02 18:45:27 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [collectiona, setCollectiona] = useState(0)
+  const [collectionb, setCollectionb] = useState(0)
+  const [collectionc, setCollectionc] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <p>Collection View</p>
+        <div className='container' >
+          <div>Item A is : {collectiona} </div>
+          <div>Item B is : {collectionb} </div>
+          <div>Item C is : {collectionc} </div>
+        </div>
+      </div>
+      <div className='cta' >
+        <button onClick={()=> setCollectiona(Math.round(Math.random()* 1000))}>A</button>
+        <button onClick={()=> setCollectionb(Math.round(Math.random()* 1000))}>B</button>
+        <button onClick={()=> setCollectionc(Math.round(Math.random()* 1000))}>C</button>
+      </div>
     </div>
   );
 }
